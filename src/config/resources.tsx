3260fdc332367a1afad437087cfcd7709ff9afca
aryan gupta <aryanguptaajj706141@gmail.com>
2026-06-30 18:56:17 +0530

    Refactor: Added Companies, Tasks in sidebar menu

--- src/config/resources.tsx
+++ src/config/resources.tsx
@@ -1,4 +1,8 @@
-import { DashboardOutlined } from "@ant-design/icons";
+import {
+  DashboardOutlined,
+  ProjectOutlined,
+  ShopOutlined,
+} from "@ant-design/icons";
 import { IResourceItem } from "@refinedev/core";
 
 export const resources: IResourceItem[] = [
@@ -8,6 +12,27 @@ export const resources: IResourceItem[] = [
     meta: {
       label: "Dashboard",
       icon: <DashboardOutlined />,
+    },
+  },
+  {
+    name: "companies",
+    list: "/companies",
+    show: "/companies/:id",
+    create: "/companies/new",
+    edit: "/companies/edit/:id",
+    meta: {
+      label: "Comapnies",
+      icon: <ShopOutlined />,
+    },
+  },
+  {
+    name: "tasks",
+    list: "/tasks",
+    create: "/tasks/new",
+    edit: "/tasks/edit/:id",
+    meta: {
+      label: "Tasks",
+      icon: <ProjectOutlined />,
     },
   },
 ];
