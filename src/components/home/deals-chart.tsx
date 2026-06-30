1242961cc334b9043d9e14dae807cd364fe14348
aryan gupta <aryanguptaajj706141@gmail.com>
2026-06-30 20:19:07 +0530

    Refactor: Add component Upcoming Events, Deals Chart in dashboard

---
+++ src/components/home/deals-chart.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const DealsChart = () => {
+  return <div>DealsChart</div>;
+};
---
+++ src/components/home/upcoming-events.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const UpcomingEvents = () => {
+  return <div>UpcomingEvents</div>;
+};
