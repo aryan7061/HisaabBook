0033530bd8b78eecbda62a52e104358a8894481d
aryan gupta <aryanguptaajj706141@gmail.com>
2026-07-01 10:54:56 +0530

    Updated component Upcoming Events in dashboard

--- src/components/home/upcoming-events.tsx
+++ src/components/home/upcoming-events.tsx
@@ -1,5 +1,67 @@
-import React from "react";
+import { CalculatorOutlined } from "@ant-design/icons";
+import { Badge, Card, List } from "antd";
+import { useList } from "@refinedev/core";
+import { Text } from "../text";
+import UpcomingEventsSkeleton from "../skeleton/upcoming-events";
+import { getDate } from "@/utilities/helpers";
+import { DASHBOARD_CALENDAR_UPCOMING_EVENTS_QUERY } from "@/graphql/queries";
+import type { Event } from "@/graphql/schema.types";
 
 export const UpcomingEvents = () => {
-  return <div>UpcomingEvents</div>;
+  const { query } = useList<Event>({
+    resource: "events",
+    pagination: { pageSize: 5 },
+    meta: {
+      gqlQuery: DASHBOARD_CALENDAR_UPCOMING_EVENTS_QUERY,
+    },
+  });
+
+  return (
+    <Card
+      style={{ height: "100%" }}
+      styles={{
+        header: { padding: "8px 16px" },
+        body: { padding: "0 1rem" },
+      }}
+      title={
+        <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+          <CalculatorOutlined />
+          <Text size="sm" style={{ marginLeft: "0.7rem" }}>
+            Upcoming Events
+          </Text>
+        </div>
+      }
+    >
+      {query.isLoading ? (
+        <List
+          itemLayout="horizontal"
+          dataSource={Array.from({ length: 5 }).map((_, index) => ({
+            id: index,
+          }))}
+          renderItem={() => <UpcomingEventsSkeleton />}
+        />
+      ) : (
+        <List
+          itemLayout="horizontal"
+          dataSource={query.data?.data || []}
+          renderItem={(item) => {
+            const renderDate = getDate(item.startDate, item.endDate);
+            return (
+              <List.Item>
+                <List.Item.Meta
+                  avatar={<Badge color={item.color} />}
+                  title={<Text size="xs">{renderDate}</Text>}
+                  description={
+                    <Text ellipsis={{ tooltip: true }} strong>
+                      {item.title}
+                    </Text>
+                  }
+                />
+              </List.Item>
+            );
+          }}
+        />
+      )}
+    </Card>
+  );
 };
--- src/components/index.tsx
+++
@@ -1,4 +0,0 @@
-import { UpcomingEvents } from "./home/upcoming-events";
-import { DealsChart } from "./home/deals-chart";
-
-export { UpcomingEvents, DealsChart };
