5410181e5cce26a6b21feba64f45c2d00c03a6e1
aryan gupta <aryanguptaajj706141@gmail.com>
2026-07-04 10:07:39 +0530

    Added 3 header cards in dashboard section.

---
+++ src/components/home/total-count-card.tsx
@@ -0,0 +1,119 @@
+import { totalCountVariants } from "@/constants";
+import { Card, Skeleton } from "antd";
+import { Text } from "../text";
+import { Area, AreaConfig } from "@ant-design/plots";
+
+type Props = {
+  resource: "companies" | "contacts" | "deals";
+  isLoading: boolean;
+  totalCount: number;
+};
+
+export const DashboardTotalCountCard = ({
+  resource,
+  isLoading,
+  totalCount,
+}: Props) => {
+  const { primaryColor, secondaryColor, icon, title } =
+    totalCountVariants[resource];
+
+  const config = {
+    data: totalCountVariants[resource].data,
+    xField: "index",
+    yField: "value",
+
+    padding: 0,
+    autoFit: true,
+    tooltip: false,
+
+    axis: {
+      x: false,
+      y: false,
+    },
+
+    theme: {
+      axis: {
+        x: {
+          grid: false,
+        },
+        y: {
+          grid: false,
+        },
+      },
+    },
+
+    shapeField: "smooth",
+
+    line: {
+      style: {
+        stroke: primaryColor,
+        lineWidth: 2,
+      },
+    },
+
+    area: {
+      style: {
+        fill: `l(270) 0:#fff 0.2:${secondaryColor} 1:${primaryColor}`,
+      },
+    },
+  };
+
+  return (
+    <Card
+      style={{ height: "96px", padding: 0 }}
+      styles={{
+        body: {
+          padding: "8px 8px 8px 12px",
+        },
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          gap: "8px",
+          whiteSpace: "nowrap",
+        }}
+      >
+        {icon}
+
+        <Text size="md" className="secondary" style={{ marginLeft: "8px" }}>
+          {title}
+        </Text>
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <Text
+          size="xxxl"
+          strong
+          style={{
+            flex: 1,
+            whiteSpace: "nowrap",
+            flexShrink: 0,
+            textAlign: "start",
+            marginLeft: "48px",
+            fontVariantNumeric: "tabular-nums",
+          }}
+        >
+          {isLoading ? (
+            <Skeleton.Button
+              style={{
+                marginTop: "8px",
+                width: "74px",
+              }}
+            />
+          ) : (
+            totalCount
+          )}
+        </Text>
+
+        <Area {...config} style={{ width: "50%" }} />
+      </div>
+    </Card>
+  );
+};
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,48 @@
-import { DealsChart, UpcomingEvents } from "@/components";
+import {
+  DashboardTotalCountCard,
+  DealsChart,
+  UpcomingEvents,
+} from "@/components";
+import { DASHBOARD_TOTAL_COUNTS_QUERY } from "@/graphql/queries";
+import { DashboardTotalCountsQuery } from "@/graphql/types";
+import { useCustom } from "@refinedev/core";
 import { Col, Row } from "antd";
 
 export const Home = () => {
+  const { query } = useCustom<DashboardTotalCountsQuery>({
+    url: "",
+    method: "get",
+    meta: {
+      gqlQuery: DASHBOARD_TOTAL_COUNTS_QUERY,
+    },
+  });
+
   return (
     <div>
+      <Row gutter={[32, 32]}>
+        <Col xs={24} sm={24} xl={8}>
+          <DashboardTotalCountCard
+            resource="companies"
+            isLoading={query.isLoading}
+            totalCount={query.data?.data.companies.totalCount ?? 0}
+          />
+        </Col>
+        <Col xs={24} sm={24} xl={8}>
+          <DashboardTotalCountCard
+            resource="contacts"
+            isLoading={query.isLoading}
+            totalCount={query.data?.data.contacts.totalCount ?? 0}
+          />
+        </Col>
+        <Col xs={24} sm={24} xl={8}>
+          <DashboardTotalCountCard
+            resource="deals"
+            isLoading={query.isLoading}
+            totalCount={query.data?.data.deals.totalCount ?? 0}
+          />
+        </Col>
+      </Row>
+
       <Row
         gutter={[32, 32]}
         style={{
@@ -23,7 +62,7 @@ export const Home = () => {
         <Col
           xs={24}
           sm={24}
-          xl={8}
+          xl={16}
           style={{
             height: "460px",
           }}
